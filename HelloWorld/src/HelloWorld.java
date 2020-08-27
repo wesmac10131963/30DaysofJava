bcc5f06774096f93b20d166d4b37048c1af8873f
Wesley McAllister <wesmac1013@gmail.com>
2020-08-27 09:18:27 -0400

    convert queue to stack

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,24 +1,28 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) throws FileNotFoundException {
-	
-		Scanner in = new Scanner(new File("students.txt"));
-		
-		List<String> students = new ArrayList<String>();
-		
-		while(in.hasNextLine()) {
-				students.add(in.nextLine());
-				
-		}	
-		for(int i = 0; i < students.size(); i++) {
-			System.out.println("name: " + students.get(i));
+		try {
+				Scanner in = new Scanner(new File("test"));
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
 		}
-		in.close();
+		LinkedList<String> names = new LinkedList<String>();
+			names.push("Caleb");
+			names.push("Sue");
+			names.push("Sally");
+			
+			System.out.println(names.remove());
+			System.out.println(names.removeFirst());
+			System.out.println(names.pop());
+
+		
 	}
 }

95a79f269d10bf288be00987023a64f08d179e73
Wesley McAllister <wesmac1013@gmail.com>
2020-08-27 08:01:23 -0400

    add input from file.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+	public static void main(String[] args) throws FileNotFoundException {
+	
+		Scanner in = new Scanner(new File("students.txt"));
+		
+		List<String> students = new ArrayList<String>();
+		
+		while(in.hasNextLine()) {
+				students.add(in.nextLine());
+				
+		}	
+		for(int i = 0; i < students.size(); i++) {
+			System.out.println("name: " + students.get(i));
+		}
+		in.close();
 	}
-
 }

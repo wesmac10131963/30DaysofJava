d54f72a9e0728e478d72fcb790a0980973be1e2f
Wesley McAllister <wesmac1013@gmail.com>
2020-08-27 13:10:25 -0400

    iterator added

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,28 +1,27 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class HelloWorld {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		try {
-				Scanner in = new Scanner(new File("test"));
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
+	public static void main(String[] args)  {
+		
 		LinkedList<String> names = new LinkedList<String>();
 			names.push("Caleb");
 			names.push("Sue");
 			names.push("Sally");
 			
-			System.out.println(names.remove());
-			System.out.println(names.removeFirst());
-			System.out.println(names.pop());
-
-		
+			ListIterator<String> it = names.listIterator();
+			it.next();
+			it.next();
+			it.add("Susan");
+			for(String s: names) {
+				System.out.println(s);
+			}
 	}
 }
